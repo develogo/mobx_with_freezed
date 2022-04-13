9063e902ac2953260dce9c81177270187cf27a23
Felipe Andrade <lippeandrade@live.com>
2022-04-13 20:10:23 -0300

    Dio injection

--- lib/app/core/services/todo_service.dart
+++ lib/app/core/services/todo_service.dart
@@ -2,11 +2,13 @@ import 'package:dio/dio.dart';
 import 'package:mobxfreezed/app/core/models/todo_model.dart';
 
 class TodoService {
-  final dio = Dio();
+  final Dio _dio;
+
+  TodoService(this._dio);
 
   Future<List<TodoModel>> getTodos() async {
     try {
-      final response = await dio.get('https://jsonplaceholder.typicode.com/todos/');
+      final response = await _dio.get('https://jsonplaceholder.typicode.com/todos/');
 
       final data = (response.data as List).map((e) => TodoModel.fromMap(e)).toList();
 
--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobxfreezed/app/core/services/todo_service.dart';
 import '../home/home_store.dart';
@@ -8,11 +9,12 @@ class HomeModule extends Module {
   @override
   final List<Bind> binds = [
     Bind.lazySingleton((i) => HomeStore(i())),
-    Bind.lazySingleton((i) => TodoService()),
+    Bind.lazySingleton((i) => TodoService(i())),
+    Bind.lazySingleton((i) => Dio()),
   ];
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute(Modular.initialRoute, child: (_, args) => HomePage()),
+    ChildRoute(Modular.initialRoute, child: (_, args) => const HomePage()),
   ];
 }

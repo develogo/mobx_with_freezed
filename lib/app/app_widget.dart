02f8717218544e25a698ca6e94069c871d65276f
Felipe Andrade <lippeandrade@live.com>
2022-04-13 23:01:10 -0300

    Update modular

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -4,9 +4,12 @@ import 'package:flutter_modular/flutter_modular.dart';
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    Modular.setInitialRoute('/');
+    return MaterialApp.router(
       title: 'Flutter Slidy',
       theme: ThemeData(primarySwatch: Colors.blue),
-    ).modular();
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+    );
   }
 }
